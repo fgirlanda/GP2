6de8bd50441b9c2c22ebb40ca1f7359cae71e9cc
fgirlanda <f.girlanda22@gmail.com>
2025-11-06 22:29:10 +0100

    metodi hash per password utente + test

--- src/gestore_password/utility/criptatore.py
+++ src/gestore_password/utility/criptatore.py
@@ -1,13 +1,14 @@
 import os
 import base64
 from argon2.low_level import hash_secret_raw, Type
+from argon2 import PasswordHasher
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 # PASSWORD SERVIZI -> AES-256 #
 
-def derive_key(password: str, salt: bytes) -> bytes:
+def derive_key(password_utente: str, salt: bytes) -> bytes:
     return hash_secret_raw(
-        secret=password.encode(),
+        secret=password_utente.encode(),
         salt=salt,
         time_cost=3,
         memory_cost=64 * 1024,  # 64MB -> resistente alle GPU
@@ -17,16 +18,34 @@ def derive_key(password: str, salt: bytes) -> bytes:
     )
 
 
-def cripta(password_chiara: str, key: bytes) -> str:
+def cripta(password_servizio: str, key: bytes) -> str:
     aes = AESGCM(key)
     nonce = os.urandom(12)
-    encrypted = aes.encrypt(nonce, password_chiara.encode(), None)
+    encrypted = aes.encrypt(nonce, password_servizio.encode(), None)
     return base64.b64encode(nonce + encrypted).decode()
 
 
-def decripta(password_cifrata: str, key: bytes) -> str:
+def decripta(password_servizio_cifrata: str, key: bytes) -> str:
     aes = AESGCM(key)
-    data = base64.b64decode(password_cifrata)
+    data = base64.b64decode(password_servizio_cifrata)
     nonce = data[:12]
     ct = data[12:]
     return aes.decrypt(nonce, ct, None).decode()
+
+# PASSWORD MASTER
+
+ph = PasswordHasher(
+time_cost=3,       # numero di iterazioni
+memory_cost=64 * 1024,  # 64 MB di RAM
+parallelism=2,     # thread
+hash_len=32,       # lunghezza hash
+salt_len=16        # salt automatico
+)
+def genera_hash(password_utente):
+    return ph.hash(password_utente)
+
+def verifica_password(password_inserita, hash_salvato):
+    try:
+        return ph.verify(hash_salvato, password_inserita)
+    except Exception:
+        return False
--- tests/test_criptatore.py
+++ tests/test_criptatore.py
@@ -1,10 +1,11 @@
-from gestore_password.utility.criptatore import derive_key, cripta, decripta
+from gestore_password.utility.criptatore import derive_key, cripta, decripta, genera_hash, verifica_password
 import os
 
+pass_utente = "password_utente"
+pass_servizio = "password_servizio"
+
 def test_aes():
-    pass_utente = "password_utente"
     salt = os.urandom(16)
-    pass_servizio = "password_servizio"
     
     key = derive_key(pass_utente, salt)
     test_cifra = cripta(pass_servizio, key)
@@ -17,4 +18,14 @@ def test_aes():
           "Salt = ", salt, "\n\n",
           "Criptata = ", test_cifra, "\n",
           "Decriptata = ", test_decifra)
+    
+def test_hash():
+    hash_corretto = genera_hash(pass_utente)
+    risultato_corretto = verifica_password(pass_utente, hash_corretto)
+    
+    hash_errato = "errore"
+    risultato_errato = verifica_password(pass_utente, hash_errato)
+    print("\n== TEST ALGORITMO HASHING ==\n\n", 
+          "Output corretto = ",risultato_corretto, "\n",
+          "Output errato = ", risultato_errato)
     

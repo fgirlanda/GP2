3bd6c6f62a9bb6c645b0b63cdedffcdc5ed8f78e
fgirlanda <f.girlanda22@gmail.com>
2025-11-07 21:41:52 +0100

    inizio refactor gestore_database

--- src/gestore_password/utility/gestore_database.py
+++ src/gestore_password/utility/gestore_database.py
@@ -3,55 +3,76 @@ import sqlite3
 import sys
 
 
-def get_dati_dir():
-    if getattr(sys, 'frozen', False):
-        base_dir =  Path(sys.executable).resolve().parent
-    else:
-        base_dir = Path(__file__).resolve().parent.parent
+# def get_dati_dir():
+#     if getattr(sys, 'frozen', False):
+#         base_dir =  Path(sys.executable).resolve().parent
+#     else:
+#         base_dir = Path(__file__).resolve().parent.parent
     
-    return base_dir / "dati"
+#     return base_dir / "dati"
 
-dati_dir = get_dati_dir()
-dati_dir.mkdir(exist_ok=True)
+# dati_dir = get_dati_dir()
+# dati_dir.mkdir(exist_ok=True)
 
 
-def inserisci_utente(utente, password_hash, salt, dbPath=None):
-    query_create = """CREATE TABLE IF NOT EXISTS Utenti(
-        utente text primary key,
-        hash_password text not null,
-        salt blob not null
-        );"""
-    quey_insert = """INSERT INTO Utenti (utente, hash_password, salt) VALUES(?, ?, ?)"""
+# def inserisci_utente(utente, password_hash, salt, dbPath=None):
+#     query_create = """CREATE TABLE IF NOT EXISTS Utenti(
+#         utente text primary key,
+#         hash_password text not null,
+#         salt blob not null
+#         );"""
+#     quey_insert = """INSERT INTO Utenti (utente, hash_password, salt) VALUES(?, ?, ?)"""
     
-    if dbPath is None:
-        dbPath = dati_dir / "utenti.db"
+#     if dbPath is None:
+#         dbPath = dati_dir / "utenti.db"
         
-    with sqlite3.connect(dbPath, uri=True) as conn:
-        cursor = conn.cursor()
-        cursor.execute(query_create)
-        cursor.execute(quey_insert, (utente, password_hash, salt))
-        conn.commit()
+#     with sqlite3.connect(dbPath, uri=True) as conn:
+#         cursor = conn.cursor()
+#         cursor.execute(query_create)
+#         cursor.execute(quey_insert, (utente, password_hash, salt))
+#         conn.commit()
         
         
-def inserisci_servizio(nome, username, password_cifrata, utente, dbPath = None):
-    query_create = """CREATE TABLE IF NOT EXISTS Servizi(
-        id integer primary key autoincrement,
-        nome text,
-        username text,
-        password_cifrata text not null
-        );"""
-    query_insert = """INSERT INTO Servizi (nome, username, password_cifrata) VALUES (?, ?, ?)"""
+# def inserisci_servizio(nome, username, password_cifrata, utente, dbPath = None):
+#     query_create = """CREATE TABLE IF NOT EXISTS Servizi(
+#         id integer primary key autoincrement,
+#         nome text,
+#         username text,
+#         password_cifrata text not null
+#         );"""
+#     query_insert = """INSERT INTO Servizi (nome, username, password_cifrata) VALUES (?, ?, ?)"""
     
-    if dbPath is None:
-        dbPath = dati_dir / f"{utente}.db"
+#     if dbPath is None:
+#         dbPath = dati_dir / f"{utente}.db"
     
-    with sqlite3.connect(dbPath, uri=True) as conn:
-        cursor = conn.cursor()
-        cursor.execute(query_create)
-        cursor.execute(query_insert, (nome, username, password_cifrata))
-        conn.commit()
+#     with sqlite3.connect(dbPath, uri=True) as conn:
+#         cursor = conn.cursor()
+#         cursor.execute(query_create)
+#         cursor.execute(query_insert, (nome, username, password_cifrata))
+#         conn.commit()
         
 
 
 # RIVEDERE IL TUTTO PIU' GENERICO (crea tabella, inserisci elemento ecc.)
+
+if getattr(sys, 'frozen', False):
+    dati_dir = Path(sys.executable).resolve().parent / "dati"
+else:
+    dati_dir = Path(__file__).resolve().parent.parent / "dati"  
+dati_dir.mkdir(exist_ok=True)
+    
+def crea_tabella(utenti: bool):
+    if utenti:
+        query_create = """CREATE TABLE IF NOT EXISTS Utenti(
+            utente text primary key,
+            hash_password text not null,
+            salt blob not null
+            );"""
+    else:
+        query_create = """CREATE TABLE IF NOT EXISTS Servizi(
+          id integer primary key autoincrement,
+          nome text,
+          username text,
+          password_cifrata text not null
+          );"""
     

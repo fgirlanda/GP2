979a2c2991b28e9c6004e840545c901a42d4a7d5
fgirlanda <f.girlanda22@gmail.com>
2025-11-08 01:04:27 +0100

    rimozione e modifica dati + test

--- src/gestore_password/utility/gestore_database.py
+++ src/gestore_password/utility/gestore_database.py
@@ -2,59 +2,6 @@ from pathlib import Path
 import sqlite3
 import sys
 
-
-# def get_dati_dir():
-#     if getattr(sys, 'frozen', False):
-#         base_dir =  Path(sys.executable).resolve().parent
-#     else:
-#         base_dir = Path(__file__).resolve().parent.parent
-    
-#     return base_dir / "dati"
-
-# dati_dir = get_dati_dir()
-# dati_dir.mkdir(exist_ok=True)
-
-
-# def inserisci_utente(utente, password_hash, salt, dbPath=None):
-#     query_create = """CREATE TABLE IF NOT EXISTS Utenti(
-#         utente text primary key,
-#         hash_password text not null,
-#         salt blob not null
-#         );"""
-#     quey_insert = """INSERT INTO Utenti (utente, hash_password, salt) VALUES(?, ?, ?)"""
-    
-#     if dbPath is None:
-#         dbPath = dati_dir / "utenti.db"
-        
-#     with sqlite3.connect(dbPath, uri=True) as conn:
-#         cursor = conn.cursor()
-#         cursor.execute(query_create)
-#         cursor.execute(quey_insert, (utente, password_hash, salt))
-#         conn.commit()
-        
-        
-# def inserisci_servizio(nome, username, password_cifrata, utente, dbPath = None):
-#     query_create = """CREATE TABLE IF NOT EXISTS Servizi(
-#         id integer primary key autoincrement,
-#         nome text,
-#         username text,
-#         password_cifrata text not null
-#         );"""
-#     query_insert = """INSERT INTO Servizi (nome, username, password_cifrata) VALUES (?, ?, ?)"""
-    
-#     if dbPath is None:
-#         dbPath = dati_dir / f"{utente}.db"
-    
-#     with sqlite3.connect(dbPath, uri=True) as conn:
-#         cursor = conn.cursor()
-#         cursor.execute(query_create)
-#         cursor.execute(query_insert, (nome, username, password_cifrata))
-#         conn.commit()
-        
-
-
-# RIVEDERE IL TUTTO PIU' GENERICO (crea tabella, inserisci elemento ecc.)
-
 if getattr(sys, 'frozen', False):
     dati_dir = Path(sys.executable).resolve().parent / "dati"
 else:
@@ -74,19 +21,6 @@ def connetti(utente_loggato: str, dbPath=None):
 
 def crea_tabella(query_create):
     global conn
-    # if not utente_loggato:
-    #     query_create = """CREATE TABLE IF NOT EXISTS Utenti(
-    #         utente text primary key,
-    #         hash_password text not null,
-    #         salt blob not null
-    #         );"""
-    # else:
-    #     query_create = """CREATE TABLE IF NOT EXISTS Servizi(
-    #         id integer primary key autoincrement,
-    #         nome text,
-    #         username text,
-    #         password_cifrata text not null
-    #         );"""
     with conn:
         cur = conn.cursor()
         cur.execute(query_create)
@@ -98,6 +32,19 @@ def inserisci_dati(query_insert, dati):
         cur = conn.cursor()
         cur.execute(query_insert, (dati[0], dati[1], dati[2]))
         conn.commit()
-        
+    
+def rimuovi_dati(query_delete, key):
+    global conn
+    with conn:
+        cur = conn.cursor()
+        cur.execute(query_delete, key)
+        conn.commit()
+
+def modifica_dati(query_update, dati):
+    global conn
+    with conn:
+        cur = conn.cursor()
+        cur.execute(query_update, dati)
+        conn.commit()
     
     
--- tests/test_gestore_database.py
+++ tests/test_gestore_database.py
@@ -1,7 +1,8 @@
 import sqlite3
-from gestore_password.utility.gestore_database import connetti, crea_tabella, inserisci_dati
+from gestore_password.utility.gestore_database import connetti, crea_tabella, inserisci_dati, rimuovi_dati, modifica_dati
 DB_MEMORY = "file::memory:?cache=shared"
 def test_utente():
+    # Connessione + creazione tabella
     connetti(None, DB_MEMORY)
     query_create_utenti = """CREATE TABLE IF NOT EXISTS Utenti(
             utente text primary key,
@@ -9,20 +10,41 @@ def test_utente():
             salt blob not null
             );"""
     crea_tabella(query_create_utenti)
+    
+    # Inserimento
     query_insert_utente = "INSERT INTO Utenti (utente, hash_password, salt) VALUES(?, ?, ?)"
     dati_utente = ["mario", "hash123", b"salt"]
     inserisci_dati(query_insert_utente, dati_utente)
-    
     conn = sqlite3.connect(DB_MEMORY, uri=True)
     cur = conn.cursor()
     cur.execute("SELECT * FROM Utenti WHERE utente = 'mario'")
     row = cur.fetchone()
-    conn.close()
-
     assert row[0] == "mario"
     assert row[1] == "hash123"
     
+    # Modifica
+    query_update_utente = "UPDATE Utenti SET utente=?, hash_password=?, salt=? WHERE utente='mario'"
+    dati_update_utente = ["luca", "hash456", b"nuovo salt"]
+    modifica_dati(query_update_utente, dati_update_utente)
+    cur.execute("SELECT * FROM Utenti WHERE utente = 'luca'")
+    row = cur.fetchone()
+
+    assert row[0] == "luca"
+    assert row[1] == "hash456"
+    
+    # Cancellazione
+    query_delete_utente = "DELETE FROM Utenti WHERE utente=?"
+    key_utente=("mario",)
+    rimuovi_dati(query_delete_utente, key_utente)
+    cur.execute("SELECT * FROM Utenti WHERE utente = 'mario'")
+    row = cur.fetchone()
+    assert row == None
+    
+    conn.close()
+
+    
 def test_servizio():
+    # Connessione + creazione tabella
     connetti("mario", DB_MEMORY)
     query_create_servizi = """CREATE TABLE IF NOT EXISTS Servizi(
             id integer primary key autoincrement,
@@ -31,6 +53,8 @@ def test_servizio():
             password_cifrata text not null
             );"""
     crea_tabella(query_create_servizi)
+    
+    # Inserimento
     query_insert_servizio = "INSERT INTO Servizi (nome, username, password_cifrata) VALUES(?, ?, ?)"
     dati_servizio = ["netflix", "mario@mail.it", "cifrata"]
     inserisci_dati(query_insert_servizio, dati_servizio)
@@ -39,8 +63,29 @@ def test_servizio():
     cur = conn.cursor()
     cur.execute("SELECT * FROM Servizi WHERE username = 'mario@mail.it'")
     row = cur.fetchone()
-    conn.close()
 
     assert row[1] == "netflix"
     assert row[2] == "mario@mail.it"
     assert row[3] == "cifrata"
+    
+    # Modifica
+    
+    query_update_servizio = "UPDATE Servizi SET nome=?, username=?, password_cifrata=? WHERE id=?"
+    dati_update_servizio = ["prime video", "luca@mail.it", "nuova cifrata", 1]
+    modifica_dati(query_update_servizio, dati_update_servizio)
+    cur.execute("SELECT * FROM Servizi WHERE username = 'luca@mail.it'")
+    row = cur.fetchone()
+
+    assert row[1] == "prime video"
+    assert row[2] == "luca@mail.it"
+    assert row[3] == "nuova cifrata"
+    
+    # Cancellazione
+    query_delete_servizio = "DELETE FROM Servizi WHERE id=?"
+    key_servizio = (1,)
+    rimuovi_dati(query_delete_servizio, key_servizio)
+    cur.execute("SELECT * FROM Servizi WHERE username='mario@mail.it'")
+    row = cur.fetchone()
+    assert row == None
+    
+    conn.close()

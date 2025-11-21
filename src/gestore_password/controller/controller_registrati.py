d54259773bbb14a4571e9d7d2708a8c0012c6ba3
fgirlanda <f.girlanda22@gmail.com>
2025-11-21 20:00:34 +0100

    fix ambiente

---
+++ src/gestore_password/controller/controller_registrati.py
@@ -0,0 +1,15 @@
+from PyQt6.QtWidgets import QMainWindow
+from views.view_registrati import Ui_MainWindow
+class Registrazione(QMainWindow, Ui_MainWindow):
+    query_registrazione = "INSERT INTO Utenti (utente, hash_password, salt) VALUES (?, ?, ?)"
+    
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.reg_btn_crea_utente.clicked.connect(self.registrati)
+    
+    def registrati(self):
+        utente = self.reg_edit_utente.text()
+        raw_password = self.reg_edit_password.text()
+        
--- src/gestore_password/controller/registrati_controller.py
+++
@@ -1,6 +0,0 @@
-from PyQt6.QtWidgets import QMainWindow
-from views import ui_registrati
-class Registrazione(QMainWindow, ui_registrati):
-    def __init__(self):
-        super.__init__()
-        self.setupUi(self)

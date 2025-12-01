301b3e1bb8f02b5eabf5e10cba15bb38d34bf854
fgirlanda <f.girlanda22@gmail.com>
2025-12-01 19:36:37 +0100

    fix test database

--- tests/test_gestore_database.py
+++ tests/test_gestore_database.py
@@ -9,15 +9,15 @@ def db():
     db.chiudi()
 
 
-def test_init(db):
+def test_init(db: GestoreDatabase):
     assert db.conn is not None
 
 
-def test_crea_utente(db):
+def test_crea_utente(db: GestoreDatabase):
     utente = "mario"
     hash_password = "prova_hash"
     salt = b"prova_salt"
     prova_dati = (utente, hash_password, salt)
     db.inserisci_utente(prova_dati)
 
-    assert db.get_utente(utente, hash_password) is not None
+    assert db.get_utente(utente) is not None
